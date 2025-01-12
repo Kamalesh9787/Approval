ce285dce4e3928ac9028e084e89f861092f3350b
Kamalesh9787 <kamaleshking957@gmail.com>
2025-01-12 08:03:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,13 +6,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
+    API_ID = int(getenv("API_ID", "25599491"))
+    API_HASH = getenv("API_HASH", "c8e3c0561cf148a6504f27b111fc3698")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     # Your Force Subscribe Channel Id Below 
     CHID = int(getenv("CHID", "")) # Make Bot Admin In This Channel
     # Admin Or Owner Id Below
-    SUDO = list(map(int, getenv("SUDO", "").split()))
+    SUDO = list(map(int, getenv("SUDO", "5983189506").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
